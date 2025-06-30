5f88d714019355b18cb28f228dcae3a03950d0a3
Murage <murage@rentwise.com>
2025-07-01 00:20:24 +0300

    Redirect logged-in users from landing page to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, :phone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :phone])
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
